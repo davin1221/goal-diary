dd75e590e43739e341b116eae57741974f32665b
Davin <kdavin1221@gmail.com>
2023-08-04 23:57:35 +0900

    에디터 편집

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -15,30 +15,49 @@ const DiaryEditor =() => {
         <section className="editor_wrapper">
             <div className="editor_date_subject">
                 <div>
+                    <span>날짜 : </span>
                     <input type="date" />
                 </div>
 
-                <div>
-                    <img src={process.env.PUBLIC_URL + `/assets/daily.png`}/>
-                    <img src={process.env.PUBLIC_URL + `/assets/study.png`}/>
-                    <img src={process.env.PUBLIC_URL + `/assets/workout.png`}/>
-                    <img src={process.env.PUBLIC_URL + `/assets/saving.png`}/>
+                <div className="editor_subject_wrapper">
+                    <span>주제 : </span>
+                    <div className="editor_subject_item">
+                        <img src={process.env.PUBLIC_URL + `/assets/daily.png`}/>
+                        <span>일상</span>
+                    </div>
+                    <div className="editor_subject_item">
+                        <img src={process.env.PUBLIC_URL + `/assets/study.png`}/>
+                        <span>공부</span>
+                    </div>
+                    <div className="editor_subject_item">
+                        <img src={process.env.PUBLIC_URL + `/assets/workout.png`}/>
+                        <span>운동</span>
+                    </div>
+                    <div className="editor_subject_item">
+                        <img src={process.env.PUBLIC_URL + `/assets/saving.png`}/>
+                        <span>저축</span>
+                    </div>
                 </div>
             </div>
 
             <div className="editor_goals">
-                <div>
-                    목표 추가 버튼 
+                <span>목표</span>
+                <MyButton text={"+"} />
+                <div className="editor_goals_area">
+                    <ul>
+                        <li>밥 먹기</li>
+                    </ul>
                 </div>
 
-                <div>
+                <div className="editor_contents_area">
+                    <span>오늘의 일기</span>
                     <textarea />
                 </div>
             </div>
 
             <div className="editor_buttons">
-                <button>작성취소</button>
-                <button>작성완료</button>
+                <MyButton text={"작성 취소"} />
+                <MyButton text={"작성 완료"} />
             </div>
         </section>
         
